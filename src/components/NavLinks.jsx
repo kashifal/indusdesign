1a265c40d8d76be8cfdef6a24742c6d3d919d7de
Kashif Sulehria <captainkashi4@gmail.com>
2023-04-05 10:37:41 +0500

    motion added

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 
+import { AnimatePresence, motion } from 'framer-motion'
 
 export function NavLinks() {
   let [hoveredIndex, setHoveredIndex] = useState(null)
