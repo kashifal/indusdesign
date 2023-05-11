9374f51515a5ef834e70f9fca04e9a4ca1cd8d53
Kashif Sulehria <65946187+kashifal@users.noreply.github.com>
2023-05-11 14:02:36 +0500

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -81,7 +81,7 @@ export default function Team() {
                 />
               
               </div>
-              <h3 className="mt-6 text-base pt-8 font-semibold leading-7 tracking-tight text-gray-900">
+              <h3 className=" text-base  font-semibold leading-7 tracking-tight text-gray-900">
                 {person.name}
               </h3>
               <p className="text-sm leading-6 text-gray-600">{person.role}</p>
