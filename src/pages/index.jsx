d707d301f56ba45c226a75f3609180a863a36948
Kashif Sulehria <65946187+kashifal@users.noreply.github.com>
2023-06-14 11:50:26 +0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -67,7 +67,7 @@ export default function Home() {
 
         <a
           href="https://github.com/kashifal/induscode"
-          className="fixed bottom-8  right-8 z-50 flex items-center gap-2 rounded-full  px-3 py-3 font-semibold ring-1 ring-transparent duration-300 bg-gray-700"
+          className="fixed bottom-8 text-white  right-8 z-50 flex items-center gap-2 rounded-full  px-3 py-3 font-semibold ring-1 ring-transparent duration-300 bg-gray-700"
         >
           
             Contribute : induscode
@@ -76,7 +76,7 @@ export default function Home() {
 
         <a
           href="mailto:ikashif@indusappps.com"
-          className="fixed bottom-8  right-24 z-50 flex items-center gap-2 rounded-full  px-3 py-3 font-semibold ring-1 ring-transparent duration-300 bg-pink-600"
+          className="fixed bottom-8  right-44 z-50 flex items-center gap-2 rounded-full  px-3 py-3 font-semibold ring-1 ring-transparent duration-300 bg-pink-600"
         >
           
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 24 24"><path fill="currentColor" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5l-8-5h16zm0 12H4V8l8 5l8-5v10z"/></svg>
